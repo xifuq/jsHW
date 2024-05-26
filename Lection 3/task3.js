d38e77fe84742ae5d1cef790a625dcacd88804b5
Vlad <nitipanovvp@elocont.ru>
2024-05-26 19:45:06 +0300

    fixed hw

--- Lection 3/task3.js
+++ Lection 3/task3.js
@@ -1,6 +1,11 @@
 // Task3**. Напишите программу, которая принимает целое положительное число n (одно любое число от 1 до 9), и выводит сумму равную 
 // n + nn + nnn, где n не перемножаются, а конкатенируются
 const sum = (n) => {
-    console.log(n + Number(String(n) + String(n)) + Number(String(n) + String(n) + String(n)))
+    if (n < 1 || n > 9) {
+        console.log("Внимательно читаем условия задачи");
+    }
+    else {
+        console.log(n + Number(String(n) + String(n)) + Number(String(n) + String(n) + String(n)))
+    }
 }
 sum(1)
